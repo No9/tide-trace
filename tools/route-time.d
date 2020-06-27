1f5fcb62ea7276b239bf8eb42c4f8e6a715684d3
Anton Whalley <anton@venshare.com>
2020-06-27 03:15:31 +0100

    added copyinstr to dtrace

--- tools/route-time.d
+++ tools/route-time.d
@@ -1,15 +1,15 @@
 #!/usr/sbin/dtrace -s
 #pragma D option quiet
 
-tideserver*:::startroute
+tide*:::startroute
 {
    track[arg2] = timestamp;
 }
 
-tideserver*:::endroute
+tide*:::endroute
 /track[arg2]/
 {
-   printf("method: %s path: %s id: %d status: %d headers:%s\n", str(arg0), str(arg1), arg2, arg3, str(arg4));
+   printf("method: %s path: %s id: %d status: %d headers:%s\n", copyinstr(arg0), copyinstr(arg1), arg2, arg3, copyinstr(arg4));
    printf("Request time %d ns\n", (timestamp - track[arg2]));
    track[arg2] = 0;
 }
